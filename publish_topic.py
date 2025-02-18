573fafe48a52887d1417bb47eafda64283191ead
ZheHe <hezhe0713@126.com>
2025-02-18 21:07:17 +0800

    add publish topic

--- publish_topic.py
+++ publish_topic.py
@@ -6,30 +6,35 @@ class MotorControlPublisher(Node):
     def __init__(self):
         super().__init__('motor_control_publisher')
         # 创建发布者，发布到 /left_hand_qpos 话题
-        self.publisher = self.create_publisher(Float64MultiArray, '/left_hand_qpos', 10)
+        self.left_publisher = self.create_publisher(Float64MultiArray, '/set_left_hand_qpos', 10)
 
-    def publish_motor_angles(self, angles):
+    def publish_left_hand_angles(self, angles):
         # 创建 Float64MultiArray 消息并将角度值填充到消息数据中
         msg = Float64MultiArray()
         msg.data = angles
 
         # 发布消息
-        self.publisher.publish(msg)
+        self.left_publisher.publish(msg)
         self.get_logger().info(f'Publishing angles: {angles}')
 
-def main():
-    rclpy.init()  # 初始化 ROS 2
-    motor_control_publisher = MotorControlPublisher()  # 创建 MotorControlPublisher 节点
+class hand_controller:
+    def __init__(self):
+        rclpy.init()  # 初始化 ROS 2
+        self.motor_control_publisher = MotorControlPublisher()  # 创建 MotorControlPublisher 节点
 
-    # 角度值 [q1, q2, q3, q4, q5, q6]
-    angles = [0, 0, 0, 3000, 0, 0]
+    def set_left_hand(self, angles):
+        # 在需要时调用函数发布消息
+        self.motor_control_publisher.publish_left_hand_angles(angles)
 
-    # 在需要时调用函数发布消息
-    motor_control_publisher.publish_motor_angles(angles)
-
-    # 关闭 ROS 2 节点
-    motor_control_publisher.destroy_node()
-    rclpy.shutdown()
+    def __del__(self):
+        # 关闭 ROS 2 节点
+        #self.motor_control_publisher.destroy_node()
+        #rclpy.shutdown()
+        pass
 
 if __name__ == '__main__':
-    main()
+    hands = hand_controller()
+
+    angles = [0.0, 0.0, 0.0, 3000.0, 0.0, 0.0]
+
+    hands.set_left_hand(angles)
